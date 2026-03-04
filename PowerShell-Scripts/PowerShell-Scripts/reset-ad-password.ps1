b42da06b434760167c147cfe72079d9377b24c95
Deepak Kumar <65071386+Deepakmalik007@users.noreply.github.com>
2026-03-04 12:44:25 +0530

    Update reset-ad-password.ps1

--- PowerShell-Scripts/PowerShell-Scripts/reset-ad-password.ps1
+++ PowerShell-Scripts/PowerShell-Scripts/reset-ad-password.ps1
@@ -1,1 +1,5 @@
+# Reset Active Directory User Password
+# This script resets a user's password in Active Directory.
+# Admin privileges required.
+
 Set-ADAccountPassword -Identity username -Reset -NewPassword (ConvertTo-SecureString "Temp@123" -AsPlainText -Force)
